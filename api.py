9e9e3ecfdbaf3be5b603f754462dc45721d493ac
Caio Carvalho <caiocarvalho.py@gmail.com>
2024-12-15 10:05:40 -0400

    fastapi

--- api.py
+++ api.py
@@ -1,27 +1,36 @@
+from fastapi import FastAPI, Query
 import requests
-import json
 
-url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
-response = requests.get(url)
-print(response)
+app = FastAPI()
 
-if response.status_code == 200:
-    dados_json = response.json()
-    dados_restaurante = {}
-    for item in dados_json:
-        nome_do_restaurante = item['Company']
-        if nome_do_restaurante not in dados_restaurante:
-            dados_restaurante[nome_do_restaurante] = []
+@app.get('/api/hello')
+def hello_world():
+    '''Enpoint de Hello!'''
+    return {'Hello': 'World'}
 
-        dados_restaurante[nome_do_restaurante].append({
-            "item": item['Item'],
-            "price": item['price'],
-            "description": item['description']
-        })
-else:
-    print(f'O erro foi {response.status_code}')
 
-for nome_do_restauranre, dados in dados_restaurante.items():
-    nome_do_arquivo = f'{nome_do_restauranre}.json'
-    with open(nome_do_arquivo, 'w') as arquivo_restaurante:
-        json.dump(dados, arquivo_restaurante, indent=4)
+@app.get('/api/restaurantes')
+def get_restaurantes(restaurante: str = Query(None)):
+    '''Endpoint que lista o cardápio dos restaurantes'''
+    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        dados_json = response.json()
+        if restaurante is None:
+            return {'dados': dados_json}
+
+        dados_restaurante = []
+        for item in dados_json:
+            if item['Company'] == restaurante:
+                dados_restaurante.append({
+                    "item": item['Item'],
+                    "price": item['price'],
+                    "description": item['description']
+                })
+        return {
+            'restaurante': restaurante,
+            'cardapio': dados_restaurante
+        }
+    else:
+        return {'erro': f'{response.status_code} - {response.text}'}
